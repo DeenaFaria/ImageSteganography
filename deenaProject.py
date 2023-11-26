b92c3940040dbf27c9cb50182e9fd8bd97b27288
Deena <deenafaria@gmailcom>
2023-11-26 19:51:14 +0600

    Updated on 11/26/2023

--- deenaProject.py
+++ deenaProject.py
@@ -138,19 +138,21 @@ def encode(input_filepath,text,output_filepath,avg):
     
 
     ijump = int(pixel_jump / width)
-    jjump = (pixel_jump - ijump * width)%width
+    
     ijump+=1
 
-    print("i,j,ijump,jjump"+str(":")+str(i)+","+str(j)+","+str(ijump)+","+str(jjump))
+    #print("i,j,ijump,jjump"+str(":")+str(i)+","+str(j)+","+str(ijump)+","+str(jjump))
     
     counter=0
     new_counter=0
     res=0
+    
 
 
     # Encoding process
     for a in range(i, height,ijump):
         b=j
+        jjump=0
         while b < width:
             pixel = img[a, b, 0]  # Seed pixel on red channel
             try:
@@ -168,6 +170,7 @@ def encode(input_filepath,text,output_filepath,avg):
 
             img[a, b, 0] = pixel
 
+            jjump = (pixel_jump - ijump * width)%width
             
             b += jjump  # Skip by pixel_jump
             counter+=1
@@ -177,7 +180,7 @@ def encode(input_filepath,text,output_filepath,avg):
                 break
 
 
-        j=0   
+        j=(b-width)%width  
      
 
         if completed:
@@ -239,14 +242,18 @@ def decode(lm,avg,input_filepath):
     res=0
     
     
+    
     for a in range(i,height,ijump):
         b=j
+        jjump=0
         while b < width:
             pixel=img[a,b,0]
            
             result += str(pixel%2)
             
             extracted_bits += 1
+
+            jjump = (pixel_jump - ijump * width)%width
 
             b=b+jjump
             counter+=1
@@ -255,7 +262,7 @@ def decode(lm,avg,input_filepath):
                 completed = True
                 break
 
-        j=0  
+        j=(b-width)%width
 
    
         if completed:
